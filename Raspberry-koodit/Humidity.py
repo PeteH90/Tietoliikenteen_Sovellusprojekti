7fe6abc9b7f37004764ebcbea4ad60a3801f16d6
JuusoIP <juuso.puolakka@hotmail.com>
2020-11-27 14:10:27 +0200

    Siivous

--- Raspberry-koodit/Humidity.py
+++ Raspberry-koodit/Humidity.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 import sys
 import Adafruit_DHT
+import paho.mqtt.client as mqtt
  
- 
-while True:
-
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-
-    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+#print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+temperature = "0"+str(temperature)
+humidity = "1"+str(humidity)
+broker_address="mqtt.eclipse.org"
+client = mqtt.Client("P1")
+client.connect(broker_address)
+client.publish("kosteus1",humidity)
+client.publish("lampotila1",temperature)
