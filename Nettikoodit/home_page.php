0dc8f266e116582f1a899be5b9a162b36e668869
Paavo Maatta <P1717@student.jamk.fi>
2020-12-21 16:27:01 +0200

    viimeisin

--- Nettikoodit/home_page.php
+++ Nettikoodit/home_page.php
@@ -1,8 +1,8 @@
 <?php
 $host='localhost';
 $dbname='cabin_monitor';
-$username='testi1';
-$password='!Anturidata123';
+$username='';
+$password='';
 $con = mysqli_connect($host,$username,$password,$dbname);
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
